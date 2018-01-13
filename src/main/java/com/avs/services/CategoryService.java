c33e3e60c8086f43d9c138fb1bfbf374e8028fa4
asmaldone <alsmaldone@yahoo.com>
2018-01-13 14:54:48 -0500

    spring restful

---
+++ src/main/java/com/avs/services/CategoryService.java
@@ -0,0 +1,15 @@
+package com.avs.services;
+
+import com.avs.api.v1.model.CategoryDTO;
+import com.avs.domain.Category;
+
+import java.util.List;
+
+public interface CategoryService {
+
+    List<CategoryDTO> getAllCategories();
+
+    CategoryDTO getCategoryByName(String name);
+}
+
+
